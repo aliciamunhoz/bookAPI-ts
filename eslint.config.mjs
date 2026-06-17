a7cb65003b85bf085d3892f6bb7b1a882db8089b
Alícia Munhóz <aliciamunhoz@MacBookPro.lan>
2026-06-17 01:40:34 +0100

    chore(tooling): ajustar setup de build, lint e testes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,22 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['src/__tests__/**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ]
